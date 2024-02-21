90eaf5e6233f486c8e097f579e5636b3726a2479
kossyak <akosyak@protonmail.com>
2024-02-21 10:31:45 +0400

    update

--- utils/index.js
+++ utils/index.js
@@ -2,11 +2,9 @@ import pako from 'pako'
 
 function compress(str) {
   const compressed = pako.deflate(str, { to: 'string' })
-  return compressed
+  return btoa(compressed.reduce((data, byte) => data + String.fromCharCode(byte), ''))
 }
-
 function decompress(compressedStr) {
-  const decompressed = pako.inflate(compressedStr.split(','), { to: 'string' })
-  return decompressed
+  return pako.inflate(atob(compressedStr).split('').map(char => char.charCodeAt(0)), { to: 'string' })
 }
 export { compress, decompress }
